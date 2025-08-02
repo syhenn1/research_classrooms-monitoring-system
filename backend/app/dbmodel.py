ea2db65df4870cfe2c3118d9e8d9a7a510249d95
syahman <RIFATOK123@GMAIL.COM>
2025-08-02 21:29:36 +0700

    a lil changes

--- backend/app/dbmodel.py
+++ backend/app/dbmodel.py
@@ -1,23 +1,31 @@
 from app import db
 from datetime import datetime
 
-class Session(db.Model):
-    __tablename__ = 'sessions'
-    session_id = db.Column(db.Integer, primary_key=True)
-    session_name = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    lecturer = db.Column(db.String(100), nullable=False)
-    subject = db.Column(db.String(100), nullable=False)
-    class_name = db.Column(db.String(100), nullable=False)
-    room = db.Column(db.String(50), nullable=False)
-
-    logs = db.relationship('Log', backref='session', lazy=True)
-
 class Log(db.Model):
     __tablename__ = 'logs'
-    id = db.Column(db.Integer, primary_key=True)
+    log_id = db.Column(db.Integer, primary_key=True)
     label = db.Column(db.String(100), nullable=False)
-    confidence = db.Column(db.Float)
-    time = db.Column(db.DateTime, default=datetime.utcnow)
+    confidence = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.String(100), nullable=False)
 
-    session_id = db.Column(db.Integer, db.ForeignKey('sessions.session_id'), nullable=False)
+
+# class Session(db.Model):
+#     __tablename__ = 'sessions'
+#     session_id = db.Column(db.Integer, primary_key=True)
+#     session_name = db.Column(db.String(100), nullable=False)
+#     date = db.Column(db.Date, nullable=False)
+#     lecturer = db.Column(db.String(100), nullable=False)
+#     subject = db.Column(db.String(100), nullable=False)
+#     class_name = db.Column(db.String(100), nullable=False)
+#     room = db.Column(db.String(50), nullable=False)
+
+#     logs = db.relationship('Log', backref='session', lazy=True)
+
+# class Log(db.Model):
+#     __tablename__ = 'logs'
+#     id = db.Column(db.Integer, primary_key=True)
+#     label = db.Column(db.String(100), nullable=False)
+#     confidence = db.Column(db.Float)
+#     time = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     session_id = db.Column(db.Integer, db.ForeignKey('sessions.session_id'), nullable=False)
