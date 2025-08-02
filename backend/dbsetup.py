e7dd9b17b472a944fea49ac10c0fdb49d04c613e
syahman <RIFATOK123@GMAIL.COM>
2025-08-02 21:00:47 +0700

    perubahan struktur, sistem migrasi dan setup lainnya.

---
+++ backend/dbsetup.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+DB_NAME = 'monitoring_system_migration'
+
+def create_database():
+    try:
+        cnx = mysql.connector.connect(user='root', password='')
+        cursor = cnx.cursor()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME} DEFAULT CHARACTER SET 'utf8'")
+        print(f"Database `{DB_NAME}` checked/created successfully.")
+        cursor.close()
+        cnx.close()
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Access denied: Check your username or password.")
+        else:
+            print(err)
+
+if __name__ == "__main__":
+    create_database()
